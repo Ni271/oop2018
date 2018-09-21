4c0ac803f0b95e59d77807f3a5622348f326ab18
Ni <dothily271@gmail.com>
2018-09-21 12:49:43 +0700

    HW1

--- src/week1/Student.java
+++ src/week1/Student.java
@@ -3,14 +3,43 @@ package week1;
 public class Student {
 
     // TODO: khai báo các thuộc tính cho Student
-
+    private String name, id, group, email;
     // TODO: khai báo các phương thức getter, setter cho Student
+    public String getName(){
+        return name;
+    }
+    public String getId(){
+        return id;
+    }
+    public String getGroup(){
+        return group;
+    }
+    public String getEmail(){
+        return email;
+    }
 
+    public void setName(String n){
+        this.name = n;
+    }
+    public void setId(String id){
+        this.id = id;
+    }
+    public void setGroup(String gr){
+        this.group = gr;
+    }
+    public void setEmail(String e){
+        this.email = e;
+    }
     /**
      * Constructor 1
      */
+
     Student() {
-        // TODO:
+        // TODO: Khởi tạo không có tham só một sinh viên
+        name = "Student";
+        id = "000";
+        group = "INT22041";
+        email = "uet@vnu.edu.vn";
     }
 
     /**
@@ -20,7 +49,11 @@ public class Student {
      * @param em
      */
     Student(String n, String sid, String em) {
-        // TODO:
+        // TODO: Khởi tạo có tham số mọt sinh viên với các thuộc tính name, id, email là các giá trị từ tham số
+        this.name = n;
+        this.id = sid;
+        this.group = "INT22041";
+        this.email = em;
     }
 
     /**
@@ -28,11 +61,19 @@ public class Student {
      * @param s
      */
     Student(Student s) {
-        // TODO:
+        // TODO: Khởi tạo sao chép (tạo ra một sinh viên có thuộc tính giống sinh viên s)
+        this.name = s.name;
+        this.id = s.id;
+        this.group = s.group;
+        this.email = s.email;
     }
 
     String getInfo() {
-        // TODO:
+        // TODO: In ra màn hình tên, mã số sinh viên, lớp, email của sinh viên
+        System.out.println(name);
+        System.out.println(id);
+        System.out.println(group);
+        System.out.println(email);
         return null; // xóa dòng này sau khi cài đặt
     }
 }
--- src/week1/StudentManagement.java
+++ src/week1/StudentManagement.java
@@ -1,24 +1,58 @@
 package week1;
 
+import javax.xml.bind.SchemaOutputResolver;
+
 public class StudentManagement {
 
     // TODO: khai báo thuộc tính students là array chứa các đối tượng thuộc lớp Student (max. 100)
+    Student[] students = new Student[100];
 
     public boolean sameGroup(Student s1, Student s2) {
-        // TODO:
-        return false; // xóa dòng này sau khi cài đặt
+        // TODO: Kiểm tra xem hai sinh viên s1 và s2 có cùng lớp hay không
+        return (s1.getGroup().equals(s2.getGroup()));
     }
 
     void studentsByGroup() {
-        // TODO:
+        // TODO: In ra danh sách sinh viên theo từng lớp
+
     }
 
     void removeStudent(String id) {
-        // TODO:
+        // TODO: Xóa sinh viên với mã số sinh viên là id ra khỏi danh sách
+
     }
 
     public static void main(String[] args) {
-        // TODO:
-        //hklhjklhjklh
+        // TODO: Tạo ra một sinh viên với thông tin của bản thân
+    Student Ly = new Student();
+    Ly.setName("Ly");
+    Ly.setId("17020873");
+    Ly.setGroup("INT220241");
+    Ly.setEmail("17020873@vnu.edu.vn");
+
+        // TODO: In ra màn hình tên và thông tin của sinh viên vừa tạo
+    System.out.println(Ly.getName());
+    Ly.getInfo();
+
+        // TODO: Tạo ba sinh viên với hai sinh vên thuộc lớp "INT2241", một sinh viên thuộc lớp "INT2242", và in ra màn hình thông tin từng sinh viên
+    Student s1 = new Student();
+    s1.getInfo();
+
+    Student s2 = new Student("Khanh", "17020673", "17020673@vnu.edu.vn");
+    s2.getInfo();
+
+    Student s3 = new Student(s1);
+    s3.setGroup("INT22042");
+    s3.getInfo();
+
+    // TODO: Kiểm tra phương thức sameGroup ở trên
+    StudentManagement management = new StudentManagement();
+
+    System.out.println("Sinh viên s1 và s2 cùng lớp: " + management.sameGroup(s1, s2));
+    System.out.println("Sinh viên s1 va s3 cùng lớp: " + management.sameGroup(s1, s3));
+    System.out.println("Sinh viên s2 và s3 cùng lớp: " + management.sameGroup(s2, s3));
+
     }
 }
+
+
